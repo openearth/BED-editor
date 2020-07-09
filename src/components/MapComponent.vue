dce6a47e1ea6113e8639cdb60412423c3b0a919d
CindyvdVries <cindyvdvries@gmail.com>
2020-07-09 15:54:53 +0200

    setup BED theming  and navigation drawer

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-mapbox
+    id="map"
+    ref="map"
+    :access-token="mapboxAccessToken"
+    :preserve-drawing-buffer="true"
+    map-style="mapbox://styles/global-data-viewer/cjtss3jfb05w71fmra13u4qqm"
+  >
+    <v-mapbox-navigation-control
+      :options="{ visualizePitch: true }"
+      position="bottom-right"
+    />
+  </v-mapbox>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      mapboxAccessToken: process.env.VUE_APP_MAPBOX_TOKEN
+    }
+  }
+}
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 100%;
+}
+
+#map .mapboxgl-ctrl-bottom-left {
+  z-index: 0;
+  right: 55px;
+  bottom: 1.5rem;
+  left: auto;
+}
+</style>
