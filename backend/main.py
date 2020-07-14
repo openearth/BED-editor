30e6eece5a715c8e7c3b447daf5df9a8faa46a27
ckrogers <rogersckw@gmail.com>
2020-07-14 11:06:39 +0200

    Add lat and lon to bbox model

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,34 @@
-from typing import List
+from typing import List, Optional
 from enum import Enum
 from fastapi import FastAPI
-from pydantic import (
-    BaseModel,
-    PositiveInt,
-    confloat,
-    conlist,
-    Schema,
-    Field
-)
+from pydantic import BaseModel, PositiveInt, confloat, conlist, Schema, Field
+
+
+class BBox(BaseModel):
+    latitude: conlist(float, min_items=2, max_items=2)
+    longitude: conlist(float, min_items=2, max_items=2)
+
 
 class SourceNameEnum(str, Enum):
     hydro_merit_1k = "hydro_merit_1k"
     hydro_merit = "hydro_merit"
 
+
 class HydroMT(BaseModel):
     # Faroe Island
-    bbox: conlist(float, min_items=4, max_items=4)
-    strord: PositiveInt
+    bbox: BBox
+    strord: Optional[PositiveInt]
     source_name: SourceNameEnum = SourceNameEnum.hydro_merit_1k
 
     class Config:
         schema_extra = {
-            'example': {
-                'bbox': [0, 0, 0, 0],
-                'strord': 4,
-                'source_name': 'hydro_merit_1k'
+            "example": {
+                "bbox": {
+                    "latitude": [62.0, 62.2],
+                    "longitude": [-7.5, -7.0]
+                },
+                "strord": 4,
+                "source_name": "hydro_merit_1k",
             }
         }
 
