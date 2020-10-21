43778262e43686dfeefd69adcd0847cc90bb0523
CindyvdVries <cindyvdvries@gmail.com>
2020-10-21 16:14:05 +0200

    clean up console.log

--- src/store/index.js
+++ src/store/index.js
@@ -31,7 +31,6 @@ export default new Vuex.Store({
           // TODO: in backedn add default value property to all fields
           Object.keys(response.components.schemas.BBox.properties).forEach(
             key => {
-              console.log(key)
               response.components.schemas.BBox.properties[key].value = 0
             }
           )
