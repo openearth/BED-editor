ca61bd29241726d03afc4a900ce7aa0ee376ce34
CindyvdVries <cindyvdvries@gmail.com>
2020-10-26 22:47:33 +0100

    if not logged in notify user in editor view

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -4,17 +4,20 @@
       <h2 class="h3 mb-3 justify-start">
         Editor
       </h2>
-      <div class="flex-grow-1 editor-content">
+      <div class="flex-grow-1 editor-content" v-if="email">
         <list-workflows v-if="$route.params.jobId === 'list'" />
         <create-workflow v-if="$route.params.jobId === 'new'" />
         <workflow-status v-if="$route.params.jobId !== 'new' && $route.params.jobId !== 'list' && $route.params.jobId" />
       </div>
-      <v-btn :to="{ name: 'editor', params: { jobId: 'new' }}" class="my-3 flex-grow-0"  outlined block>
+      <v-btn v-if="email" :to="{ name: 'editor', params: { jobId: 'new' }}" class="my-3 flex-grow-0" outlined block>
         Create new workflow
       </v-btn>
-      <v-btn :to="{ name: 'editor', params: { jobId: 'list' }}" class="flex-grow-0"  outlined block>
+      <v-btn v-if="email" :to="{ name: 'editor', params: { jobId: 'list' }}" class="flex-grow-0" outlined block>
         List workflows
       </v-btn>
+      <div v-else class="account-details__placeholder">
+        <p>Please login to show workflows and create new.</p>
+      </div>
     </v-container>
 
   </v-navigation-drawer>
@@ -24,11 +27,20 @@
 import CreateWorkflow from '../components/CreateWorkflow'
 import ListWorkflows from '../components/ListWorkflows'
 import WorkflowStatus from '../components/WorkflowStatus'
+import { mapState } from 'vuex'
+import _ from 'lodash'
+
 export default {
   components: {
     CreateWorkflow,
     WorkflowStatus,
     ListWorkflows
+  },
+  computed: {
+    ...mapState(['user']),
+    email () {
+      return _.get(this.user, 'email')
+    }
   },
   data () {
     return {
@@ -46,6 +58,7 @@ export default {
 .editor-content {
   width: 100%;
   max-height: 100%;
+  height: 100%;
   overflow-y: auto;
   overflow-x: hidden;
 }
